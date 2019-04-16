cd60ca1ec985df1745510f6b2815ed22fee5fe8d
guy guy <you@example.com>
2019-04-16 12:51:05 +0300

    Add the RobotMap changes

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,13 +14,42 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+  //PORTS FOR THE TALONS THAT DRIVING
+  public static final int RIGHT_MASTER_PORT=1;
+  public static final int RIGHT_SLAVE_PORT=2;
+  public static final int LEFT_MASTER_PORT=3;
+  public static final int LEFT_SLAVE_PORT=4;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+  //PORTS FOR THE TALONS THAT ELEVATORING
+  public static final int MASTER_ELEVATOR_PORT=5;
+  public static final int SLAVE_ELEVATOR_PORT=6;
+
+  //PORTS FOR THE COLLECTOR TALON
+  public static final int COLLECTOR_PORT=7;
+
+  //PORTS FOR JOYSTICKS AND CONTROLLERS
+  public static final int PORT_JOYSTICK_LEFT=0;
+  public static final int PORT_JOYSTICK_RIGHT=1;
+  public static final int PORT_XBOX_CONTROLLER=2;
+
+  //PORTS FOR BUTTONS
+  public static final int A_BUTTON_PORT=1;
+  public static final int B_BUTTON_PORT=2;
+  public static final int X_BUTTON_PORT=3;
+  public static final int Y_BUTTON_PORT=4;
+  public static final int LEFT_BUMPER_PORT=5;
+  public static final int RIGHT_BUMPER_PORT=6;
+
+  //Limit Switch port
+  public static final int LIMIT_SWITCH_PORT=1;
+
+  //Speed constatns
+  public static final double MOVE=0.7;
+  public static final double STOP=0;
+
+  //Compressor port
+  public static final int PORT_COMPRESSOR=0;
+
+  //DoubleSelonoides ports
+  public static final int[] DOUBLE_SELONOIDES_PORTS= {0,1};
 }
