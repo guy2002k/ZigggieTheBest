f4926702d44f86c986cd89126806dc284cb43cf7
guy guy <you@example.com>
2019-04-18 16:20:31 +0300

    Add and fix CollectorBalls subsytem

--- src/main/java/frc/robot/subsystems/CollectorBalls.java
+++ src/main/java/frc/robot/subsystems/CollectorBalls.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 import frc.robot.commands.CollectBall;
