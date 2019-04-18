4b0ef6de880d55839e35aee4e1b6a3d7c30259b1
guy guy <you@example.com>
2019-04-18 15:21:40 +0300

    Add open piston and close piston buttons

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -12,9 +12,11 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.ClosePiston;
 import frc.robot.commands.ForceMode;
 import frc.robot.commands.LiftDown;
 import frc.robot.commands.LiftUp;
+import frc.robot.commands.OpenPiston;
 import frc.robot.commands.VelocityMode;
 
 /**
