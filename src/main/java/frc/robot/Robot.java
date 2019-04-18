182c8489b260c0637dbf90efe9dab50d0551c66b
guy guy <you@example.com>
2019-04-18 12:46:05 +0300

    Add the collectorBalls subsystem instance

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Chassis;
+import frc.robot.subsystems.CollectorBalls;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.ExampleSubsystem;
 
@@ -29,6 +30,7 @@ public class Robot extends TimedRobot {
   public static OI oi;
   public static Chassis chassis;
   public static Elevator elevator;
+  public static CollectorBalls collectorBalls;
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
@@ -42,6 +44,7 @@ public class Robot extends TimedRobot {
     oi = new OI();
     chassis=Chassis.getInstance();
     elevator=Elevator.getInstance();
+    collectorBalls=CollectorBalls.getInstance();
     m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
