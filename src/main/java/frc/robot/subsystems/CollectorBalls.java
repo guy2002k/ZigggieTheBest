74dbcdaa06cbe5651e12ee7048b4d4b0cea345f5
guy guy <you@example.com>
2019-04-18 13:52:37 +0300

    Change the default commad in CollectorBalls subsystem

--- src/main/java/frc/robot/subsystems/CollectorBalls.java
+++ src/main/java/frc/robot/subsystems/CollectorBalls.java
@@ -14,6 +14,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
+import frc.robot.commands.CollectBall;
 
 /**
  * Add your docs here.
@@ -51,6 +52,6 @@ public class CollectorBalls extends Subsystem {
 
   @Override
   public void initDefaultCommand() {
-    //nothing
+    setDefaultCommand(new CollectBall());
   }
 }
