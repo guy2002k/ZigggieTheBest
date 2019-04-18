041a37ae1ea75bfc0fd3111fb45c9ea04ea4973f
guy guy <you@example.com>
2019-04-18 13:51:53 +0300

    Add and Fix the CollectBall command

---
+++ src/main/java/frc/robot/commands/CollectBall.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.CollectorBalls;
+
+public class CollectBall extends Command {
+  public CollectBall() {
+    requires(Robot.collectorBalls);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if(Robot.oi.getLeftTriggerSpeed()>Robot.oi.getLeftTriggerSpeed())
+      Robot.collectorBalls.PushIn(Robot.oi.getLeftTriggerSpeed());
+    else if(Robot.oi.getLeftTriggerSpeed()<Robot.oi.getLeftTriggerSpeed())
+      Robot.collectorBalls.PushOut(Robot.oi.getRightTriggerSpeed());
+    else
+      Robot.collectorBalls.stop();   
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.collectorBalls.stop();
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
